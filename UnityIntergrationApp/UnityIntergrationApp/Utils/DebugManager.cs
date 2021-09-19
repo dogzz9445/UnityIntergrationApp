c6f017115a8e0786db460cbd92e7d0aad776418a
Dongmin Jang (장동민) <37837785+dogzz9445@users.noreply.github.com>
2021-09-19 18:34:11 +0900

    commit

---
+++ UnityIntergrationApp/UnityIntergrationApp/Utils/DebugManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityIntergrationApp.Utils
+{
+    public sealed class DebugManager
+    {
+        private DebugManager() { }
+        private static readonly Lazy<DebugManager> g_instance = new Lazy<DebugManager>();
+        private static bool m_isDebug;
+
+        public static bool IsDebug { get => m_isDebug; set => m_isDebug = value; }
+        public static DebugManager Instance { get => g_instance.Value; }
+    }
+}
